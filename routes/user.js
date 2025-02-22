d4c80f1a1d891021c5115649db6358a2d484e9b5
Ashabh <mohdashabh@gmail.com>
2025-02-22 15:27:13 +0530

    Posts.js/ Get post

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const post = require('../models/Post')
